4f0f01e4d358794bcd8240f650452583131980ce
soaika74 <hoviethung10b2@gmail.com>
2021-07-21 17:21:24 +0700

    upload

---
+++ Modules/ProductType/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('producttype::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('producttype.name') !!}
+    </p>
+@endsection
---
+++ Modules/UserGroup/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('usergroup::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('usergroup.name') !!}
+    </p>
+@endsection
