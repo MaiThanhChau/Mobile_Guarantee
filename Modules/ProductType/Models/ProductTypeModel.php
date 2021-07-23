406140f7d6dc5ba3b3fa692e7b309e37c58b2422
QuangDat95 <quangdat13sk@gmail.com>
2021-07-23 11:58:35 +0700

    update softdelete

--- Modules/ProductType/Models/ProductTypeModel.php
+++ Modules/ProductType/Models/ProductTypeModel.php
@@ -2,8 +2,11 @@
 namespace Modules\ProductType\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class ProductTypeModel extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $table = 'product_groups';
 }
