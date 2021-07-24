9aca84eb21f0af783d64190a07259c4c48721189
hoan-blip <hoqhoan6868@gmailcom>
2021-07-24 12:01:11 +0700

    dasboard

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -10,4 +10,7 @@ class AdminController extends Controller
     public function index(){
         return view('layout.admin.index');
     }
+    public function dashboard(){
+        return view('layout.admin.dashboard');
+    }
 }
